f93246f37a67f7c98f1c1486ed3d5d5a5a873637
HONG DIEP <nguyenmaihongdiep@gmail.com>
2026-03-25 13:29:36 +0700

    Cập nhật thêm một dòng in ra "Hello again!" trong file HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //update hello again
+        System.out.println("Hello again!");
     }
 }
